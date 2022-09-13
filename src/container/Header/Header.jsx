a017b4e93a0a147355f2ca4e99954851dcc5aaa3
unknown <dabrettwestwood@gmail.com>
2022-09-13 13:48:27 -0400

    first commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components'
+
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+       <SubHeading title='Chase the new flavor'/>
+       <h1 className='app__header-h1'>
+         The Key To Fine Dining
+       </h1>
+       <p className='p__opensans' style={{ margin: '2rem 0'}}>
+          Try out Tampa's best kept secret to see what you have been missing out on! <br/>
+          Seafood done like never before. <br/>
+          Open 7 days a week
+       </p>
+       <button type='button' className='custom__button'>
+          Explore Menu
+       </button>
+    </div>
+    <div className='app__wrapper_img'>
+       <img src={images.welcome} alt='header img' />
+    </div>
+  </div>
+);
+
+export default Header;
